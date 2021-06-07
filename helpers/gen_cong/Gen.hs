6ab3ff8ad293301360ebd46041023eb6e1e8dd07
DwarfMaster <luc@dwarfmaster.net>
2021-06-07 10:06:04 +0200

    Add support for dependent term in congruence generation

--- helpers/gen_cong/Gen.hs
+++ helpers/gen_cong/Gen.hs
@@ -18,7 +18,8 @@ instance Show Sort where
 
 data Arg = Simple Sort
          | Dep Int Sort
-         | Term Int Int
+         | TermDep Int Int
+         | Term ([String] -> String)
 
 data AccT = AccT
           { val  :: String
@@ -41,13 +42,18 @@ genIType termf typef ((Dep d s, name) : tl) acc =
      <> "(P _ _ (Pack' " <> (fromString $ orig $ acc !! d) <> " " <> (fromString $ val $ acc !! d) <> ") "
      <> "(λ p, H' (" <> fromString name <> "1 (projT1 p)) (" <> fromString name <> " (projT2 p)))) "
   <> "(λ _, " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> "))"
-genIType termf typef ((Term d t, name) : tl) acc =
+genIType termf typef ((TermDep d t, name) : tl) acc =
   "P _ _ (P _ _ " <> (fromString $ val $ acc !! d) <> " " <> (fromString $ val $ acc !! t) <> ") "
   <> "(λ " <> (fromString name) <> ", P _ _ "
      <> "(P _ _ (Pack' " <> (fromString $ orig $ acc !! d) <> " " <> (fromString $ val $ acc !! d) <> ") "
      <> "(λ p, HEq' _ (" <> (fromString $ orig $ acc !! t) <> " (projT1 p)) "
         <> "(" <> (fromString $ val $ acc !! t) <> " (projT2 p)) "
         <> "(" <> fromString name <> "1 (projT1 p)) (" <> fromString name <> " (projT2 p)))) "
+  <> "(λ _, " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> "))"
+genIType termf typef ((Term tf, name) : tl) acc =
+  let typ = fromString $ tf $ fmap val acc in
+  "P _ _ " <> typ <> " "
+  <> "(λ(" <> (fromString name) <> " : ε _ (" <> typ <> ")), P _ _ (H' " <> fromString name <> "1 " <> fromString name <> ") "
   <> "(λ _, " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> "))"
 
 genCong :: Builder -> ([String] -> String) -> ([String] -> String) -> [(Arg,String)] -> [AccT] -> Builder
@@ -58,25 +64,40 @@ genCong tabs termf typef ((Simple _, name) : tl) acc =
     tabs <> "(λ _ " <> hT <> ", rewr (heq_to_eq " <> hT <> ")\n"
     <> tabs <> "  (λ " <> t <> ", " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> ")\n"
     <> genCong (tabs <> "  ") termf typef tl (AccT (name <> "1") (name <> "1") : acc) <> ")"
-genCong tabs termf typef ((Dep _ _, name) : tl) acc =
+genCong tabs termf typef ((Dep dt ds, name) : tl) acc =
   let t = fromString name :: Builder in
   let hT = fromString ('h' : name) :: Builder in
-    tabs <> "(λ " <> t <> " " <> hT <> ", rewr (packExtT " <> fromString name <> "1 " <> t <> " " <> hT <> ")\n"
+    tabs <> "(λ " <> t <> " " <> hT <> ", @rewr _ _ _ " <> fromString name <> "1 " <> fromString name <> " "
+    <> "(packExtT " <> fromString name <> "1 " <> t <> " " <> hT <> ")\n"
     <> tabs <> "  (λ " <> t <> ", " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> ")\n"
     <> genCong (tabs <> "  ") termf typef tl (AccT (name <> "1") (name <> "1") : acc) <> ")"
-genCong tabs termf typef ((Term td tt, name) : tl) acc =
+genCong tabs termf typef ((TermDep td tt, name) : tl) acc =
   let t = fromString name :: Builder in
   let hT = fromString ('h' : name) :: Builder in
     tabs <> "(λ(" <> t <> " : ε _ (P _ _ " <> (fromString $ val $ acc !! td) <> " " <> (fromString $ val $ acc !! tt) <> ")) " <> hT
        <> ", rewr (packExt " <> fromString name <> "1 " <> t <> " " <> hT <> ")\n"
     <> tabs <> "  (λ " <> t <> ", " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> ")\n"
     <> genCong (tabs <> "  ") termf typef tl (AccT (name <> "1") (name <> "1") : acc) <> ")"
+genCong tabs termf typef ((Term tf, name) : tl) acc =
+  let t = fromString name :: Builder in
+  let hT = fromString ('h' : name) :: Builder in
+    tabs <> "(λ " <> fromString name <> " " <> hT <> ", @rewr _ _ " <> (fromString $ tf $ map val acc) <> " "
+    <> fromString name <> "1 " <> fromString name <> " (heq_to_eq " <> hT <> ")\n"
+    <> tabs <> "  (λ " <> t <> ", " <> genIType termf typef tl (AccT name (name <> "1") : acc) <> ")\n"
+    <> genCong (tabs <> "  ") termf typef tl (AccT (name <> "1") (name <> "1") : acc) <> ")"
 
+t0 :: [a] -> a
+t0 lst = (reverse lst) !! 0
+t1 :: [a] -> a
+t1 lst = (reverse lst) !! 1
 
 generators :: M.Map String ([(Arg,String)],[String] -> String,[String] -> String)
 generators = M.fromList
            [ ("prod", ([(Simple (SVar "s"), "A"), (Dep 0 (SVar "s'"), "B")], \[b, a] -> "(P s s' " <> a <> " " <> b <> ")", \_ -> "(u (smax s s'))"))
-           , ("lambda", ([(Simple (SVar "s"), "A"), (Dep 0 (SVar "s'"), "B"), (Term 1 0, "t")], \[t, _, _] -> t, \[_, b, a] -> "(P _ _ " <> a <> " " <> b <> ")"))
+           , ("lambda", ([(Simple (SVar "s"), "A"), (Dep 0 (SVar "s'"), "B"), (TermDep 1 0, "t")], \[t, _, _] -> t, \[_, b, a] -> "(P _ _ " <> a <> " " <> b <> ")"))
+           , ("app", ([(Simple (SVar "s"), "A"), (Dep 0 (SVar "s'"), "B"), (Term (\l -> "(P _ _ " <> t0 l <> " " <> t1 l <> ")"), "f"), (Term t0, "a")],
+                      \[x,f,_,_] -> "(" <> f <> " " <> x <> ")",
+                      \[x,_,b,_] -> "(" <> b <> " " <> x <> ")"))
            ]
 
 main :: IO ()
